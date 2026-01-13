8388c4177b85b9521e8b0b697a208ebb1264ec50
thangnd4 <thangnd4@TMS-THANGND4>
2026-01-13 16:21:04 +0700

    Add project files.

---
+++ src/ElasticLoggingOption.cs
@@ -0,0 +1,29 @@
+﻿using Serilog.Events;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StaxiLogging.src
+{
+    public class ElasticLoggingOption
+    {
+        public string Uri { get; set; } = "http://localhost:9200";
+        public string User { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+        public string EnvironmentName { get; set; } = string.Empty;
+        public string IndexFormat { get; set; } = "staxi-log-app-{0:yyyy.MM.dd}";
+        public string ApplicationName { get; set; } = "StaxiApp";
+        public bool EnableLogConsole { get; set; } = false;
+        // Mini log
+        public LogEventLevel MiniLogLevel { get; set; } = LogEventLevel.Information;
+        public string PathFileSinkFail { get; set; }
+        //Index
+        public bool AutoRegisterTemplate { get; set; } = false;
+        public int NumberOfReplicas { get; set; } = 1;
+        public int NumberOfShards { get; set; } = 1;
+        public int BatchPostingLimit { get; set; }
+
+
+    }
+
+}
---
+++ src/SerilogElasticExtension.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Serilog;
+using Serilog.Events;
+using Serilog.Sinks.Elasticsearch;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+using Serilog.Sinks.File;
+namespace StaxiLogging.src
+{
+    public static class SerilogElasticExtension
+    {
+        public static LoggerConfiguration UseElasticLoggingConfig(this LoggerConfiguration logger, ElasticLoggingOption options)
+        {
+            return logger
+                .MinimumLevel.Debug()
+                .MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
+                .MinimumLevel.Override("Microsoft.AspNetCore", LogEventLevel.Warning)
+                .MinimumLevel.Override("System", LogEventLevel.Warning)
+                .MinimumLevel.Override("Microsoft.EntityFrameworkCore", LogEventLevel.Warning)
+
+                .Enrich.FromLogContext()
+                .Enrich.WithProperty("Application", options.ApplicationName)
+                .Enrich.WithProperty("Environment", options.EnvironmentName)
+
+                  .WriteTo.Elasticsearch(new ElasticsearchSinkOptions(new Uri(options.Uri))
+                  {
+                      AutoRegisterTemplate = options.AutoRegisterTemplate,
+                      IndexFormat = options.IndexFormat,
+                      NumberOfReplicas = options.NumberOfReplicas,
+                      NumberOfShards = options.NumberOfShards,
+                      AutoRegisterTemplateVersion = AutoRegisterTemplateVersion.ESv7,
+                      MinimumLogEventLevel = options.MiniLogLevel,
+                      BatchPostingLimit = options.BatchPostingLimit,
+
+                      ModifyConnectionSettings = conn =>
+                        conn.BasicAuthentication(options.User,
+                         options.Password
+                        ),
+
+                      EmitEventFailure = EmitEventFailureHandling.WriteToSelfLog |
+                       EmitEventFailureHandling.WriteToFailureSink,
+
+                      FailureSink = new FileSink(
+                        options.PathFileSinkFail,
+                        new Serilog.Formatting.Json.JsonFormatter(),
+                        null
+                        )
+                  });
+        }
+
+    }
+}
